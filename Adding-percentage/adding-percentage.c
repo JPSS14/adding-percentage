f876a49dc00608c866bd4f884319e818c8875873
JPSS14 <jp_scarabelli@hotmail.com>
2020-11-17 21:43:32 -0300

    adding-percentage UPDATE 1: Update README

--- Adding-percentage/adding-percentage.c
+++ Adding-percentage/adding-percentage.c
@@ -3,17 +3,19 @@
 
 int main(){
 	int op;
-	float valor, novo_valor=0, debito=1.99/100, credito_vista=4.74/100, credito_14d=3.79/100, credito_30d=3.03/100;
-	
+	float valor, novo_valor=0, debito=1.99/100, credito_vista=4.74/100, credito_14d=3.79/100, credito_30d=3.03/100, credito_2x=7.69/100;
 	do{
-		printf("Digite o valor da peca\n");
+		printf("Digite o valor da peca: R$ ");
 		scanf("%f",&valor);
 		printf("\nValor credito a vista: 		R$ %.2f  acrescimo de + R$ %.2f", valor+(valor*credito_vista), valor*credito_vista);
+		printf("\nValor credito em 2x: 		R$ %.2f  acrescimo de + R$ %.2f", valor+(valor*credito_2x), valor*credito_2x);
 		printf("\nValor credito a 14 dias:	R$ %.2f  acrescimo de + R$ %.2f", valor+(valor*credito_14d), valor*credito_14d);
 		printf("\nValor credito a 30 dias: 	R$ %.2f  acrescimo de + R$ %.2f", valor+(valor*credito_30d), valor*credito_30d);
 		printf("\nValor debito: 			R$ %.2f  acrescimo de + R$ %.2f", valor+(valor*debito), valor*debito);
-		printf("\n\n\nDeseja verificar outra peca digite 1");
-		printf("\nPara finalizar digite 0\n");
+		printf("\n\n\nDigite uma opcao");
+		printf("\n1 - Verificar outra peca");
+		printf("\n0 - Para finalizar\n");
+		printf("Opcao: ");
 		scanf("%d",&op);
 	}while(op!=0);
 
--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # adding-percentage
 Programa em C, que ajuda a adicionar o valor da tarifa de uma máquina de cartão em um produto.
+
+O programa solicita ao usuário o valor de um determinado produto, e em seguida são realizados os cálculos baseados em tarifas reais de uma máquina de cartões. Quando os cálculos são finalizados, o programa retorna o valor adicionando a tarifa para alguns tipos de operações disponíveis na máquina.
+
+Em seguida o usuário tem a opção de digitar um novo valor, ou finalizar o programa.
+
+![adding-percentage](https://user-images.githubusercontent.com/40327303/99467239-c07a5d80-291c-11eb-811a-6fc5e7551cd4.PNG)
+
